fc74beaf2e9f9abb196f6698c1e5f8ef971962c4
Joshua Toney <josh@Joshuas-MacBook-Air.local>
2023-10-09 20:27:52 -0400

    Update footer links

--- components/ui/Footer/FooterLinks.tsx
+++ components/ui/Footer/FooterLinks.tsx
@@ -16,17 +16,17 @@ export const FooterLinks = () => {
           <li className="py-3 md:py-0 md:pb-4">
             <Link
               href="/"
-              className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+              className="text-white transition duration-150 ease-in-out hover:text-zinc-300"
             >
               About
             </Link>
           </li>
           <li className="py-3 md:py-0 md:pb-4">
             <Link
-              href="/"
-              className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+              href="https://bear-bull-bot.gitbook.io/bear-bull-bot/"
+              className="text-white transition duration-150 ease-in-out hover:text-zinc-300"
             >
-              Careers
+              Docs
             </Link>
           </li>
           <li className="py-3 md:py-0 md:pb-4">
@@ -42,14 +42,14 @@ export const FooterLinks = () => {
       <div className="col-span-1 lg:col-span-2">
         <ul className="flex flex-col flex-initial md:flex-1">
           <li className="py-3 md:py-0 md:pb-4">
-            <p className="font-bold text-white transition duration-150 ease-in-out hover:text-zinc-200">
+            <p className="font-bold text-white transition duration-150 ease-in-out hover:text-zinc-300">
               LEGAL
             </p>
           </li>
           <li className="py-3 md:py-0 md:pb-4">
             <Link
               href="/"
-              className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+              className="text-white transition duration-150 ease-in-out hover:text-zinc-300"
             >
               Privacy Policy
             </Link>
@@ -57,7 +57,7 @@ export const FooterLinks = () => {
           <li className="py-3 md:py-0 md:pb-4">
             <Link
               href="/"
-              className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+              className="text-white transition duration-150 ease-in-out hover:text-zinc-300"
             >
               Terms of Use
             </Link>
